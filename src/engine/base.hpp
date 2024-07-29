58bd0919f018f96e6b744709593ad60653cd4fed
Humza Qureshi <30800959+humz2k@users.noreply.github.com>
2024-07-29 17:55:49 -0500

    windows_compatability

--- src/engine/base.hpp
+++ src/engine/base.hpp
@@ -10,6 +10,10 @@
 #include <typeinfo>
 #include <unordered_map>
 
+#ifndef M_PI
+    #define M_PI 3.14159265358979323846
+#endif
+
 namespace SPRF {
 
 static int GetDisplayWidth() {
--- src/engine/renderer.hpp
+++ src/engine/renderer.hpp
@@ -614,10 +614,11 @@ class Renderer : public Logger {
         m_skybox_model =
             std::make_shared<raylib::Model>(raylib::Mesh::Cube(1, 1, 1));
         m_skybox_model->materials[0].shader = m_skybox_shader;
+        int temp = MATERIAL_MAP_CUBEMAP;
         SetShaderValue(m_skybox_model->materials[0].shader,
                        GetShaderLocation(m_skybox_model->materials[0].shader,
                                          "environmentMap"),
-                       (int[1]){MATERIAL_MAP_CUBEMAP}, SHADER_UNIFORM_INT);
+                       &temp, SHADER_UNIFORM_INT);
         raylib::Image img(path);
         m_skybox_model->materials[0].maps[MATERIAL_MAP_CUBEMAP].texture =
             LoadTextureCubemap(img, CUBEMAP_LAYOUT_AUTO_DETECT);
